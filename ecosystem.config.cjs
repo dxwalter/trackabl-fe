d2314f693692b687924c9fc5b6b74070e367b652
Daniel Walter <31169881+dxwalter@users.noreply.github.com>
2024-06-15 22:17:52 +0100

    added pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,20 +2,10 @@ module.exports = {
   apps: [
     {
       name: "Tracakbl-FE",
-      script: "./node_modules/nuxt/bin/nuxt.js",
-      args: "start",
+      port: "3000",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
     },
   ],
-  deploy: {
-    // "production" is the environment name
-    development: {
-      user: "web",
-      host: ["178.128.165.230"],
-      ref: "origin/master",
-      repo: "git@git-repo.git",
-      ssh_options: ["ForwardAgent=yes"],
-      path: "/var/www/trackabl-frontent/actions-runner/_work/trackabl-fe/trackabl-fe",
-      "post-deploy": "pm2 startOrRestart ecosystem.config.cjs --env production",
-    },
-  },
 };
